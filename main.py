16b43959e75b53fe64ac42451202fe75954eadff
Yusuf <43914317+yusufornek@users.noreply.github.com>
2022-09-27 17:44:58 +0000

    added

---
+++ main.py
@@ -0,0 +1,15 @@
+#Pseudo code.
+Start
+Sit on the chair
+Open the computer
+Look at the charge indicator
+if the computer has a charge
+  open windows mail
+else the computer has no charge
+  charge the computer
+There are new mails
+if there are
+  read all new mails
+else there are not
+  close the mail
+Finish

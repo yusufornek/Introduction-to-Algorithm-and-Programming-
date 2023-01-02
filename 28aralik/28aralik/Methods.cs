1f999f628cc15c534d4057d48d9895628f249a42
Yusuf <43914317+yusufornek@users.noreply.github.com>
2023-01-02 10:59:59 +0300

    updated
    updated

---
+++ 28aralik/28aralik/Methods.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28aralik
+{
+    internal class Methods
+    {
+        public static void yazdir(int[] dizimiz)
+        {
+            foreach (int i in dizimiz)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void kareler(int[] arrays)
+        {
+            foreach (int i in arrays)
+            {
+                Console.WriteLine("Karesi : " + (i*i));
+            }
+        }
+
+        public static void bakiyebak(double birikmis)
+        {
+            Console.WriteLine("Birikmiş paranız : " + birikmis);
+        }
+        public static double cikanTutar(double birikmis, double cikan)
+        {
+            double kalan_tutar = 0;
+            if(birikmis < cikan)
+            {
+                Console.WriteLine("Yetersiz bakiye");
+                kalan_tutar = birikmis;
+            }
+            else
+            {
+                kalan_tutar = birikmis - cikan;
+                Console.WriteLine(kalan_tutar);
+            }
+            return kalan_tutar;
+        }
+        public static double paraEkle(double birikmis, double yatacakpara)
+        {
+            double yenibakiye = birikmis + yatacakpara;
+            Console.WriteLine("Eski bakiyeniz : " + birikmis);
+            Console.WriteLine("Yeni bakiyeniz : " + yenibakiye);
+            return yenibakiye;
+        }
+
+    }
+}
---
+++ 28aralik/28aralik/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28aralik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] notlar = new int[5];
+            for (int i = 0; i < notlar.Length; i++)
+            {
+                notlar[i] = 60 + (i * 10);
+            }
+            /*Methods.yazdir(notlar);
+            Methods.kareler(notlar);
+            Console.ReadLine();*/
+            double birikmis = 5000;
+            string secim;
+
+            do
+            {
+                Console.WriteLine("ATM uygulamasına hoş geldiniz!");
+                Console.WriteLine("Birikmiş parayı görüntülemek için 1'e basınız.");
+                Console.WriteLine("Para çıkarmak için 2'ye basınız.");
+                Console.WriteLine("Para eklemek için 3'ye basınız.");
+                Console.WriteLine("Çıkış yapmak için 4'ye basınız.");
+
+                 secim = Console.ReadLine();
+
+                switch (secim)
+                {
+                    case "1":
+                        Methods.bakiyebak(birikmis);
+                        break;
+                    case "2":
+                        Console.WriteLine("Çıkarmak istediğiniz tutarı giriniz : ");
+                        double cikan = double.Parse(Console.ReadLine());
+                       
+                        birikmis =Methods.cikanTutar(birikmis, cikan);
+                        break;
+                    case "3":
+                        Console.WriteLine("Kaç lira yatırmak istiyorsunuz?");
+                        double yatacakpara = double.Parse(Console.ReadLine());
+                        birikmis = Methods.paraEkle(birikmis, yatacakpara);
+                        break;
+                    case "4":
+                        Console.WriteLine("Başarılı olarak çıkış yaptınız");
+                        break;
+                }
+            } while (secim != "4");
+            Console.ReadLine();
+        }
+    }
+}

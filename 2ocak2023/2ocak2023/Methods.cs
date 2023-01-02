cbaa9fc36fb31f04a5071e549900241c892f81e6
Yusuf <43914317+yusufornek@users.noreply.github.com>
2023-01-02 10:57:15 +0300

    Add files via upload

---
+++ 2ocak2023/2ocak2023/Methods.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2ocak2023
+{
+    internal class Methods
+    {
+        public static void deneme()
+        {
+            Console.WriteLine("Günü giriniz : ");
+            int gun = int.Parse(Console.ReadLine());
+            switch (gun)
+            {
+                case 1:
+                    Console.WriteLine("Pazartesi");
+                    break;
+                case 2:
+                    Console.WriteLine("Salı");
+                    break;
+                case 3:
+                    Console.WriteLine("Çarşamba");
+                    break;
+                case 4:
+                    Console.WriteLine("Perşembe");
+                    break;
+                case 5:
+                    Console.WriteLine("Cuma");
+                    break;
+                case 6:
+                    Console.WriteLine("Cumartesi");
+                    break;
+                case 7:
+                    Console.WriteLine("Pazar");
+                    break;
+                default: 
+                    Console.WriteLine("Geçersiz gün değeri");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2ocak2023/2ocak2023/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2ocak2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods.deneme();
+        }
+    }
+}

09b7ebda332ec3b64fc0b484e77baa0f1ffa92c8
Yusuf <43914317+yusufornek@users.noreply.github.com>
2023-01-02 11:00:28 +0300

    updated
    updated

---
+++ AlgoritmaDers4/AlgoritmaDers4/Program.cs
@@ -0,0 +1,116 @@
+﻿// See https://aka.ms/new-console-template for more information//
+//Console.WriteLine("Hello, World!");//
+
+/*
+int sayi = 45;
+Console.WriteLine(sayi);
+int sayi2 = 90;
+Console.Write(sayi2);
+
+long sayi3 = 464684646545;
+Console.WriteLine(sayi3);
+long sayi4 = 356543455344;
+Console.WriteLine(sayi4);
+
+double sayi5 = 7545468;
+Console.WriteLine(sayi5);
+double sayi6 = 5684465;
+Console.WriteLine(sayi6);
+
+char harf1 = 'a';
+Console.WriteLine(harf1);
+char harf2 = 'b';
+Console.WriteLine(harf2);
+
+string cumle1 = "merhaba";
+Console.WriteLine(cumle1);
+string cumle2 = "yazilimci";
+Console.WriteLine(cumle2);
+-------------------------------------
+int first = 345;
+int second = 895;
+int toplam = first + second;
+Console.WriteLine("Toplam değeri : " + toplam);
+
+int cikarma = first - second;
+Console.WriteLine("Fark : " + cikarma);
+
+int carp = first * second;
+Console.WriteLine("Çarpım : " + carp);
+
+int bolu = first / second;
+Console.WriteLine("Bölme İşlemi : " + bolu);
+--------------------------------------------------------------
+
+
+double taban = 14.3;
+double yukseklik = 8.74;
+double alan = (taban * yukseklik) / 2;
+Console.WriteLine("Üçgenin alanı : " + alan);
+--------------------------------------------------------------
+
+double vize = 78;
+double final = 83;
+double ort = vize * 0.4 + final * 0.6;
+Console.WriteLine("Notlar ortalaması :\n" + ort);
+
+-------------------------------------------------------------
+double ilkSinav = 78;
+double ikinciSinav = 92;
+double ucuncuSinav = 84;
+
+double ort = (ilkSinav + ikinciSinav + ucuncuSinav) / 3;
+Console.WriteLine("Ortalama : " + ort);
+
+double ort4 = (ort * 4) / 100;
+Console.WriteLine("4'lük sistemdeki karşılığı : " + ort4);
+---------------------------------------------------------------
+
+
+
+double h = 3.4;
+double r = 2.3;
+double pi = 3.14;
+
+double hacim = pi * r * r * h;
+double yuzeyAlani = 2 * pi * r * h;
+Console.WriteLine("Hacim : " + hacim);
+Console.WriteLine("Yüzey Alanı : " + yuzeyAlani
+
+
+
+Console.WriteLine("Sayi1'i giriniz : ");
+int sayi1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Sayi2'i giriniz : ");
+int sayi2 = int.Parse(Console.ReadLine());
+
+int toplam = sayi1 + sayi2;
+Console.WriteLine( "Sayıların Toplamı : " + toplam );  
+
+int cikar = sayi1 - sayi2;
+Console.WriteLine("Sayıların Farkı : " + cikar);
+
+int carp = sayi1 * sayi2;
+Console.WriteLine("Sayıların Çarpımı : " + carp);
+
+*/
+
+
+Console.WriteLine("Üçgenin yüksekliğini giriniz : ");
+int yukseklik = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Üçgenin taban uzunluğunu giriniz : ");
+int taban = int.Parse(Console.ReadLine());
+
+int alan = yukseklik * taban / 2;
+Console.WriteLine("Üçgenin alanı : " + alan);
+
+
+
+
+
+
+
+
+
